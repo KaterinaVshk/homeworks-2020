4f44e45f0842bc2a6b549945bf1e8fd3a67b9dde
KaterinaVshk <veo_25@mail.com>
2020-03-22 19:47:37 +0300

    Add the second task solutions

---
+++ HW02/Ekaterina_Vashkevich/ekaterina_vashkevich_hw_02_t_02.rb
@@ -0,0 +1,12 @@
+# writes lines from file to array
+logs = File.readlines('data_t_02.txt')
+
+REGEX_POST = /(.+)[\s\-]{5,}\[(.+)\] "POST (.+) HTTP/;
+
+#formated line from log, that match REGEX_POST
+def formated_log(log)
+    log.select {|line| line.match(REGEX_POST)}.map { |line| 
+    "#{line.match(REGEX_POST)[2]} FROM: #{line.match(REGEX_POST)[1]} TO: #{line.match(REGEX_POST)[3].upcase}"}
+end
+
+puts formated_log(logs)
