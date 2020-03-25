2dd87594d565152f9d5d53e569ed61255116bb86
KaterinaVshk <veo_25@mail.com>
2020-03-25 20:59:23 +0300

    Fix with rubocop

--- HW02/Ekaterina_Vashkevich/ekaterina_vashkevich_hw_02_t_02.rb
+++ HW02/Ekaterina_Vashkevich/ekaterina_vashkevich_hw_02_t_02.rb
@@ -1,10 +1,9 @@
 # writes lines from file to array
 logs = File.readlines('data_t_02.txt')
-IP = /\d+.\d+\.\d+\.\d+/.freeze
-DATA = /\[\d{2}\/\w+\/\d{4}\:\d{2}\:\d{2}\:\d{2} \+\d{4}\]/.freeze
-PATH = /\/\w+\/\w+\/\w+/.freeze
-REGEX_POST = /#{IP} - - #{DATA} "POST #{PATH}/.freeze
-#REGEX_POST = /#{IP}#{DATA} "POST #{PATH} HTTP/.freeze
+IP = %r{/\d+.\d+\.\d+\.\d+/}.freeze
+DATA = %r{/\[\d{2}\/\w+\/\d{4}\:\d{2}\:\d{2}\:\d{2} \+\d{4}\]/}.freeze
+PATH = %r{/\/\w+\/\w+\/\w+/}.freeze
+REGEX_POST = %r{/#{IP} - - #{DATA} "POST #{PATH}/}.freeze
 
 # formated line from log, that match REGEX_POST
 def formated_log(log)
