31618d11af2de71b894da4c43c7401bedb046705
KaterinaVshk <veo_25@mail.com>
2020-03-25 20:38:31 +0300

    Edit regular expression in task 2

--- HW02/Ekaterina_Vashkevich/ekaterina_vashkevich_hw_02_t_02.rb
+++ HW02/Ekaterina_Vashkevich/ekaterina_vashkevich_hw_02_t_02.rb
@@ -1,15 +1,15 @@
 # writes lines from file to array
 logs = File.readlines('data_t_02.txt')
-
-REGEX_POST = /(.+)[\s\-]{5,}\[(.+)\] "POST (.+) HTTP/.freeze
+IP = /\d+.\d+\.\d+\.\d+/.freeze
+DATA = /\[\d{2}\/\w+\/\d{4}\:\d{2}\:\d{2}\:\d{2} \+\d{4}\]/.freeze
+PATH = /\/\w+\/\w+\/\w+/.freeze
+REGEX_POST = /#{IP} - - #{DATA} "POST #{PATH}/.freeze
+#REGEX_POST = /#{IP}#{DATA} "POST #{PATH} HTTP/.freeze
 
 # formated line from log, that match REGEX_POST
 def formated_log(log)
   log.select { |line| line.match(REGEX_POST) }.map do |line|
-    data = line.match(REGEX_POST)[2]
-    ip = line.match(REGEX_POST)[1]
-    path = line.match(REGEX_POST)[3]
-    "#{data} FROM: #{ip} TO: #{path.upcase}"
+    "#{line[DATA]} FROM: #{line[IP]} TO: #{line[PATH].upcase}"
   end
 end
 
