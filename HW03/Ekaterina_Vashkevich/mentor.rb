3941b76532b1b3c13ffd104fbb6ce8698bec2d03
KaterinaVshk <veo_25@mail.com>
2020-04-01 01:21:47 +0300

    Edit class Mentor

--- HW03/Ekaterina_Vashkevich/mentor.rb
+++ HW03/Ekaterina_Vashkevich/mentor.rb
@@ -1,7 +1,7 @@
 # class describes the state and behavior of mentors and
 # interaction with students
 class Mentor
-  attr_reader :name, :surname, :notifications, :students
+  attr_reader :name, :surname, :notifications
 
   def initialize(name, surname)
     @name = name
@@ -10,20 +10,25 @@ class Mentor
     @students = []
   end
 
-  def subscribe_to(student)
+  def subscribe_to!(student)
     @students << student
-    student.subscribe(self)
+    student.mentors << self
+    subscribe_notification(student)
   end
 
-  def notify_homeworks(student, homework)
-    @notifications << "#{student.name} #{student.surname} add #{homework}"
-  end
-
-  def check_homeworks(student, homework)
-    student.notify_check_homework(homework)
+  def check_homeworks(student, number_homework)
+    student.notifications << Notification.new("'#{number_homework}' checked")
   end
 
   def read_notifications!
     @notifications.clear
   end
+
+  private
+
+  def subscribe_notification(student)
+    notification = Notification.new("#{name} #{surname} subscribe to you")
+    student.notifications << notification
+  end
 end
+
