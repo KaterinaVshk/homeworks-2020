fbf0577e445bdadadc529bff5110a6313a7c6898
KaterinaVshk <veo_25@mail.com>
2020-04-01 01:23:56 +0300

    Edit app.rb (add 2 class)

--- HW03/Ekaterina_Vashkevich/app.rb
+++ HW03/Ekaterina_Vashkevich/app.rb
@@ -1,24 +1,31 @@
 require_relative 'student'
 require_relative 'mentor'
+require_relative 'homework'
+require_relative 'notification'
+require 'pry'
 
 student = Student.new('John', 'Doe')
 mentor = Mentor.new('Jack', 'Gonsales')
 
-student.sumbit_homework!('Homework 1')
-p student.homeworks             # ["Homework 1"]
+student.sumbit_homework!('HW01', 'sorting algorithm', 'http://github...')
+p student.homeworks.join(', ') # "{HW01, sorting algorithm, http://github...}"
 
-mentor.subscribe_to(student)
-p student.notifications         # ["Jack Gonsales subscribe to you"]
-p mentor.notifications          # []
+mentor.subscribe_to!(student)
+p student.notifications.join(',') # "Jack Gonsales subscribe to you "
+p mentor.notifications.join(',') # Output: ""
 
-student.sumbit_homework!('Homework 2')
-p mentor.notifications          # ["John Doe add Homework 2"]
+student.sumbit_homework!('HW02', 'working with file', 'http://github...')
+p mentor.notifications.join(', ') # "Student: John Doe add 'HW02' ...'"
+p student.homeworks.join(', ') # "{HW01, sorting ...}, {HW02, working...}"
 
 mentor.check_homeworks(student, 'Homework 2')
-p student.notifications         # ["Jack Gonsales sub...","Homework 2 checked"]
+p student.notifications.join(', ') # "Jack Gonsales sub.., Homework: 'Home.."
 
 mentor.read_notifications!
-p mentor.notifications          # []
+p mentor.notifications.join(', ') # Output: ""
 
 student.read_notifications!
-p student.notifications         # []
+p student.notifications.join(', ') # Output: ""
+
+
+
