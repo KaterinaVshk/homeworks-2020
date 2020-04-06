a659c0f9652e2cabddc2f3e8149877f0d1519490
KaterinaVshk <veo_25@mail.com>
2020-04-06 20:05:14 +0300

    Add my module

---
+++ HW04/Ekaterina_Vashkevich/lib/my_module.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+# Module contain my implement each,map and select
+module MyModule
+    def my_each
+      if block_given?
+        index = 0
+        while index < length
+          yield(self[index])
+          index += 1
+        end
+        self
+      else
+        to_enum(__method__)
+      end
+    end
+  
+    def my_map
+      if block_given?
+          rezult =[]
+          index = 0
+          while index < length
+            rezult << yield(self[index])
+            index += 1
+          end
+          rezult
+        else
+          to_enum(__method__)
+      end
+    end
+  
+    def my_select
+      if block_given?
+          rezult =[]
+          index = 0
+          while index < length
+            rezult << self[index] if yield(self[index])
+            index += 1
+          end
+          rezult
+        else
+          to_enum(__method__)
+      end
+    end
+end
+  
+  
